10db5f9d381ac424b03382fda727840caf3f2dbe
9021007 <24487638+9021007@users.noreply.github.com>
2024-07-10 04:34:01 -0700

    MySQL db switch

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import random
 import os
 import subprocess
 import time
+import mysql.connector
+
+db_address = "ADDRESS"
+db_port = "1234"
+db_user = "USER"
+db_password = "PASSWORD"
+
+db_name = "marketpi_data"
+db_table = "marketplace_table"
 
 count = subprocess.Popen(["timeout", "--preserve-status", "5", "tcpdump", "-i", "bluetooth0"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 initial = count.stdout.read().decode("utf-8").split("\n")[4].split(" ")[0]
@@ -10,6 +19,8 @@ count2 = subprocess.Popen(["timeout", "--preserve-status", "5", "tcpdump", "-i",
 final = count2.stdout.read().decode("utf-8").split("\n")[4].split(" ")[0]
 print(int(final) - int(initial))
 
-with open("devices.txt", "a") as f:
-    f.write("{ \"Time\": " + str(time.time()) + ", \"Count\": " + str(int(final) - int(initial)) + "},\n")
-    f.close()
+db = mysql.connector.connect(user=db_user, password=db_password, host=db_address, database=db_name, port=db_port)
+cursor = db.cursor()
+cursor.execute("INSERT INTO " + db_table + " (packets, timestamp) VALUES (" + str(int(final) - int(initial)) + ", NOW());")
+db.commit()
+cursor.close()
